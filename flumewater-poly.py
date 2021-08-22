ba186dbaf840294f9c63004f41a5d77848040ef2
JimBo <jimboca3@gmail.com>
2021-08-21 18:01:14 -0700

    Fix controller name

--- flumewater-poly.py
+++ flumewater-poly.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     try:
         polyglot = Interface([Controller])
         polyglot.start()
-        control = Controller(polyglot, 'controller', 'controller', 'Airscape')
+        control = Controller(polyglot, 'controller', 'controller', 'FlumeWater')
         polyglot.runForever()
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
